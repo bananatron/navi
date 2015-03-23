232c9d09b82a64e34e9d070fb5e79e4dcca4fcd9
bananatron <bananatrongo@gmail.com>
2015-03-23 18:58:05 +0000

    added limited test

--- routes/index.js
+++ routes/index.js
@@ -58,7 +58,41 @@ router.post('/message', function(req, res, next) {
   res.send(p.key());
 });
 
-
+router.get('/messagetest', function(req, res, next){
+  ranguid = (Math.random() * 1500+10).toString();
+  ranfail = Math.floor(Math.random()+0.5);
+  console.log(ranfail);
+  if ( ranfail == 1 ){
+    end_state = "success";
+    end_err = "";
+  }else {
+    end_state = "failed";
+    end_err = "Purpose failed from test."
+  }
+  
+  ll = myFirebaseRef.push({
+    date: "3/20/2015 4:59:43 PM",
+    request_name: "Test Message",
+    msg_guid: Math.floor(ranguid),
+    msg_body: "ProjectNumber : 161616\\r\\nCISNumber : \\r\\nCISVerificationDate : \\r\\nCISExpiryDate : \\r\\nCISRate : \\r\\nAddressNumber : 123454\\r\\nAlphaName : Aa Test Vendor 4\\r\\nTaxID : 462382666\\r\\nTaxExempt : 6693056708\\r\\nVerificationNumber : V1001782304\\r\\nSocialSecurity : \\r\\nWithholdingPercent : 20\\r\\nSelfCertificationAllowed : Y\\r\\nRegistration Number : 0234324\\r\\nAddressLine1 : 222 Highland Dr\\r\\nAddressLine2 : Hendglade House\\r\\nAddressLine3 : 46 New Road\\r\\nAddressLine4 : \\r\\nCity : Sourdough\\r\\nState : CA\\r\\nCounty : West Midlands\\r\\nCountry : \\r\\nZipCode : DY8 1PA\\r\\n",
+    state: "pending", 
+    err: ""
+  });
+  
+  setTimeout(function(){
+    ll.set({
+      date: "3/20/2015 4:59:43 PM",
+      request_name: "Test Message",
+      msg_guid: Math.floor(ranguid),
+      msg_body: "ProjectNumber : 161616\\r\\nCISNumber : \\r\\nCISVerificationDate : \\r\\nCISExpiryDate : \\r\\nCISRate : \\r\\nAddressNumber : 123454\\r\\nAlphaName : Aa Test Vendor 4\\r\\nTaxID : 462382666\\r\\nTaxExempt : 6693056708\\r\\nVerificationNumber : V1001782304\\r\\nSocialSecurity : \\r\\nWithholdingPercent : 20\\r\\nSelfCertificationAllowed : Y\\r\\nRegistration Number : 0234324\\r\\nAddressLine1 : 222 Highland Dr\\r\\nAddressLine2 : Hendglade House\\r\\nAddressLine3 : 46 New Road\\r\\nAddressLine4 : \\r\\nCity : Sourdough\\r\\nState : CA\\r\\nCounty : West Midlands\\r\\nCountry : \\r\\nZipCode : DY8 1PA\\r\\n",
+      state: end_state, 
+      err: end_err
+    });
+}, 6000);
+  
+  
+  //res.render('index'); 
+});
 
 //GET Message (for testing)
 // router.get('/message', function(req, res, next) {
